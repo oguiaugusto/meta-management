e4278b0f54016932cce14886cd85e678df4a696f
Guilherme <guilhaugusto05@gmail.com>
2025-12-02 22:46:39 -0300

    add user icon and dropdown menu to header

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import logo from '../../assets/logo.svg';
+import { LogOut, Settings, UserRound } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import logo from '../../assets/logo.svg';
 
 const Header: React.FC = () => {
   const navButtons = [
@@ -22,6 +31,8 @@ const Header: React.FC = () => {
     },
   ];
 
+  const iconColor = 'oklch(27.4% 0.006 286.033)';
+
   return (
     <header className="w-full h-15 flex flex-col justify-between items-center bg-zinc-200">
       <div className="h-full flex justify-between items-center w-full px-10">
@@ -39,6 +50,29 @@ const Header: React.FC = () => {
             }
           </ul>
         </nav>
+        <div className="w-[168px] flex items-center justify-end">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="cursor-pointer focus-visible:outline-none">
+              <UserRound size={ 30 } strokeWidth={ 1.5 } color={ iconColor } />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-40 bg-zinc-50" align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuItem>
+                <a href="/user" className="flex items-center gap-2 w-full">
+                  <Settings size={ 10 } strokeWidth={ 1.5 } color={ iconColor } />
+                  <span className="mb-[1px]">Settings</span>
+                </a>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <button className="m-0 p-0 border-none outline-none flex items-center gap-2 w-full cursor-pointer">
+                  <LogOut size={ 10 } strokeWidth={ 1.5 } color={ iconColor } />
+                  <span className="mb-[1px]">Sign out</span>
+                </button>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
       <div className="border-b border-slate-300 w-full" />
     </header>
