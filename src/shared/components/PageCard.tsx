814fb346a077b3777f781ddf6c8f9985653be285
Guilherme <guilhaugusto05@gmail.com>
2025-12-06 19:07:43 -0300

    PageCard header can have description or not

--- src/shared/components/PageCard.tsx
+++ src/shared/components/PageCard.tsx
@@ -1,14 +1,41 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import React, { type PropsWithChildren } from 'react';
 
-type Props = PropsWithChildren & { title: string };
+type Props = PropsWithChildren & {
+  title: string;
+  description?: string;
+};
 
 const PageCard: React.FC<Props> = (p) => {
+  const renderCardHeader = () => {
+    let titleClass = 'text-xl ';
+
+    let description: React.ReactNode | null = (
+      <CardDescription className="mb-1">{ p.description }</CardDescription>
+    );
+
+    if (p.description) {
+      titleClass += 'text-left';
+    } else {
+      titleClass += 'text-center';
+      description = null;
+    }
+
+    console.log(titleClass);
+
+    return (
+      <CardHeader>
+        <div>
+          <CardTitle className={ titleClass }>{ p.title }</CardTitle>
+          { description }
+        </div>
+      </CardHeader>
+    );
+  };
+
   return (
     <Card className="w-4xl mt-8 bg-zinc-50 pb-10">
-      <CardHeader>
-        <CardTitle className="text-center text-xl">{ p.title }</CardTitle>
-      </CardHeader>
+      { renderCardHeader() }
       <CardContent>{ p.children }</CardContent>
     </Card>
   );
