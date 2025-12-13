d8602af28675f2b60e14292fd258b56dc0c9b455
Guilherme <guilhaugusto05@gmail.com>
2025-12-13 18:25:42 -0300

    update messages object

--- backend/src/shared/constants/messages.ts
+++ backend/src/shared/constants/messages.ts
@@ -3,4 +3,5 @@ export const MESSAGES = {
   uniqueEmail: 'Email is already registered',
   wrongCredentials: 'Username/email or password is incorrect',
   unauthorized: 'Unauthorized',
+  passwordReset: 'If an account is registered for this email, you\'ll recieve a password reset email shortly',
 };
