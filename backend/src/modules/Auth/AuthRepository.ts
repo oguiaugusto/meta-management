71d57fe6e24a5b7c04b48f3705cf9f2b5f3f6410
Guilherme <guilhaugusto05@gmail.com>
2025-12-13 20:32:49 -0300

    weird spacing on AuthRepository methods

--- backend/src/modules/Auth/AuthRepository.ts
+++ backend/src/modules/Auth/AuthRepository.ts
@@ -2,15 +2,15 @@ import { prisma } from '../../../database/prisma';
 import { PasswordResetDTO, RefreshTokenDTO, UserRepositoryDTO } from './types';
 
 class AuthRepository {
-  public async createUser (data: UserRepositoryDTO) {
+  public async createUser(data: UserRepositoryDTO) {
     return prisma.user.create({ data });
   }
 
-  public async findUserByUsername (username: string) {
+  public async findUserByUsername(username: string) {
     return prisma.user.findUnique({ where: { username } });
   }
 
-  public async findUserByEmail (email: string) {
+  public async findUserByEmail(email: string) {
     return prisma.user.findUnique({ where: { email } });
   }
 
@@ -25,33 +25,33 @@ class AuthRepository {
     });
   }
 
-  public async createRefreshToken (data: RefreshTokenDTO) {
+  public async createRefreshToken(data: RefreshTokenDTO) {
     await prisma.$transaction([
       prisma.refreshToken.deleteMany({ where: { userId: data.userId } }),
       prisma.refreshToken.create({ data }),
     ]);
   }
 
-  public async findRefreshToken (tokenHash: string) {
+  public async findRefreshToken(tokenHash: string) {
     return prisma.refreshToken.findUnique({ where: { tokenHash } });
   }
 
-  public async deleteRefreshToken (tokenHash: string) {
+  public async deleteRefreshToken(tokenHash: string) {
     await prisma.refreshToken.deleteMany({ where: { tokenHash } });
   }
 
-  public async createPasswordReset (data: PasswordResetDTO) {
+  public async createPasswordReset(data: PasswordResetDTO) {
     await prisma.$transaction([
       prisma.passwordReset.deleteMany({ where: { userId: data.userId } }),
       prisma.passwordReset.create({ data }),
     ]);
   }
 
-  public async findPasswordReset (tokenHash: string) {
+  public async findPasswordReset(tokenHash: string) {
     return prisma.passwordReset.findUnique({ where: { tokenHash } });
   }
 
-  public async deletePasswordReset (tokenHash: string) {
+  public async deletePasswordReset(tokenHash: string) {
     await prisma.passwordReset.deleteMany({ where: { tokenHash } });
   }
 }
