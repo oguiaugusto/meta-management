accb7a4a2a594a0dba2a980c1d4fa4e16c8ccf9c
Guilherme <guilhaugusto05@gmail.com>
2025-12-13 21:28:22 -0300

    update messages object

--- backend/src/shared/constants/messages.ts
+++ backend/src/shared/constants/messages.ts
@@ -3,7 +3,7 @@ export const MESSAGES = {
   uniqueEmail: 'Email is already registered',
   wrongCredentials: 'Username/email or password is incorrect',
   unauthorized: 'Unauthorized',
-  sentPasswordReset: 'If an account is registered for this email, you\'ll recieve a password reset email shortly',
+  sentPasswordReset: 'If an account is registered with this email, you\'ll recieve a password reset email shortly',
   invalidPasswordReset: 'The password reset link is invalid. Please request a new one',
   expiredPasswordReset: 'The password reset link has expired. Please request a new one.',
   successPasswordReset: 'Your password has been successfully updated',
