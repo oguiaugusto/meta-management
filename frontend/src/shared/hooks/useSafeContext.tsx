fdd53e1ee8c9ef5b4c70c259625ead3d32f389f3
Guilherme <guilhaugusto05@gmail.com>
2025-12-14 15:38:10 -0300

    useSafeContext hook

---
+++ frontend/src/shared/hooks/useSafeContext.tsx
@@ -0,0 +1,13 @@
+import { useContext, Context } from 'react';
+
+function useSafeContext<T>(context: Context<T | null>): T {
+  const ctx = useContext(context);
+
+  if (!ctx) {
+    throw new Error('useSafeContext must be used within a Provider with a valid value');
+  }
+
+  return ctx;
+}
+
+export { useSafeContext };
