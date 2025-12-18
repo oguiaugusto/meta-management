d74b0c126673c820d2c745a46c03629577eec34a
Guilherme <guilhaugusto05@gmail.com>
2025-12-18 16:52:17 -0300

    useErrorAlert hook to have empty message instead of null

--- frontend/src/shared/hooks/useErrorAlert.tsx
+++ frontend/src/shared/hooks/useErrorAlert.tsx
@@ -3,17 +3,17 @@ import { Alert, AlertDescription } from '@/components/ui/alert';
 import { X } from 'lucide-react';
 
 const useErrorAlert = () => {
-  const [message, setMessage] = useState<string | null>(null);
+  const [message, setMessage] = useState<string>('');
 
   const renderAlert = () => {
-    if (message === null) return null;
+    if (!message) return null;
 
     return (
       <Alert variant="default" className="mb-3 bg-red-400 flex items-center justify-between gap-2">
         <AlertDescription className="text-zinc-100">{ message }</AlertDescription>
         <button
           className="cursor-pointer text-zinc-100 focus-visible:outline-none hover:text-zinc-200"
-          onClick={ () => setMessage(null) }
+          onClick={ () => setMessage('') }
         >
           <X size={ 16 } />
         </button>
