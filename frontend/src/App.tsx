8607d2a769e566dfbe5b175b32b9c8282239890b
Guilherme <guilhaugusto05@gmail.com>
2025-12-18 21:50:50 -0300

    install tanstack query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthRedirect } from './shared/components/AuthRedirect';
 import { Home } from './modules/Home/pages/Home';
 import { Login } from './modules/User/pages/Login';
@@ -8,6 +9,8 @@ import { Databases } from './modules/Database/pages/Databases';
 import { Database } from './modules/Database/pages/Database';
 import { AuthProvider } from './shared/contexts/AuthContext';
 import { Toaster } from './components/ui/sonner';
+
+const queryClient = new QueryClient();
 
 const renderRouter = (
   path: string,
@@ -24,20 +27,21 @@ const renderRouter = (
 }
 
 const App: React.FC = () => {
-
   return (
-    <AuthProvider>
-      <Toaster position="top-center" />
-      <Router>
-        <Routes>
-          { renderRouter('/login', <Login />) }
-          { renderRouter('/register', <Register />) }
-          { renderRouter('/', <Home />, true) }
-          { renderRouter('/databases', <Databases />, true) }
-          { renderRouter('/databases/:id', <Database />, true) }
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <QueryClientProvider client={ queryClient }>
+      <AuthProvider>
+        <Toaster position="top-center" />
+        <Router>
+          <Routes>
+            { renderRouter('/login', <Login />) }
+            { renderRouter('/register', <Register />) }
+            { renderRouter('/', <Home />, true) }
+            { renderRouter('/databases', <Databases />, true) }
+            { renderRouter('/databases/:id', <Database />, true) }
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 };
 
