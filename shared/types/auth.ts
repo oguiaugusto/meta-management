179c8e82d8c9f083f80972738646860c39b406d3
Guilherme <guilhaugusto05@gmail.com>
2025-12-20 16:54:14 -0300

    move auth types to shared folder between back and front and export them accordingly

---
+++ shared/types/auth.ts
@@ -0,0 +1,22 @@
+export type UserDTO = {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+};
+
+export type UserRepositoryDTO = Omit<UserDTO, 'password'> & {
+  passwordHash: string;
+}
+
+export type RefreshTokenDTO = {
+  userId: string;
+  tokenHash: string;
+  expiresAt: string;
+};
+
+export type PasswordResetDTO = {
+  userId: string;
+  tokenHash: string;
+  expiresAt: string;
+};
